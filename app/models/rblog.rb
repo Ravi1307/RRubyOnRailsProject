0253d90e445dad271bd0264003ea7605632674cc
Ravikanth <ravi13071995@gmail.com>
2016-07-13 13:54:34 +0530

    Initial Commit

--- app/models/rblog.rb
+++ app/models/rblog.rb
@@ -1,8 +1,8 @@
 class Rblog < ActiveRecord::Base
   
-  scope :recent, lambda { |a, l, o| ((a == 'all') ? order("rblogs.id DESC").offset(o).limit(l) : where(:author => a).order("rblogs.id DESC").limit(l).offset(o)) };
+  scope :recent, lambda { |a, l, o| ((a == 'all') ? order(:id => :desc).offset(o).limit(l) : where(:author => a).order(:id => :desc).limit(l).offset(o)) };
   scope :post_titles, lambda { |author| where(:author => author) };
-  scope :authors, lambda { pluck('DISTINCT author') };
+  scope :authors, lambda { distinct.pluck(:author) };
   scope :count_authors, lambda { |author| where(:author => author).count };
   
 end
